7b25e1ba010ad75d24795c140be8a43ea5b26864
mDheoAdji <m.dheo008@gmail.com>
2026-06-22 19:40:48 +0700

    ford commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    "https://sistem-pakar-prokrastinasi.vercel.app/api",
 });
 
 export default api;
